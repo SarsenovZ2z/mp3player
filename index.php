e0ed106a0c6d676bf0476de14bd327d91c4772cb
SarsenovZ2z <nurik9293709@gmail.com>
2018-03-14 02:40:20 +0600

    mp3Player v2

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Mp3player by Z2z</title>
         <link rel="stylesheet" type="text/css" href="css/main.css">
+        <script>
+            var samples = [
+                <?php
+                    $dir = "samples";
+                    $samples = opendir($dir);
+                    while (($file = readdir($samples))!==false) {
+                        if (!is_dir($file)) {
+                            echo "\"$file\",\n";
+                        }
+                    }
+                ?>
+            ];
+        </script>
     </head>
     <body>
         <header></header>
@@ -12,12 +28,41 @@
             <div id="mp3-player">
                 <canvas id="audio-visualizer"></canvas>
                 <div id="audio-box">
-                    <audio controls autoplay﻿></audio>
+                    <audio controls loop></audio>
+                    <div id="playlist">
+                        <select id="player-navigator">
+                            <script>
+                                samples.forEach(function(e) {
+                                    document.write("<option value='samples/"+e+"'>"+e+"</option>");
+                                });
+                            </script>
+                        </select>
+                    </div>
+                    <div id="upload-navigator">
+                        <input id="uploadMp3" type="file" accept=".mp3"/>
+                        <button onclick = "uploadMp3File()">Upload</button>
+                    </div>
                 </div>
             </div>
         </main>
         <footer>
             <script src="scripts/main.js"></script>
+            <script>
+                document.getElementById("player-navigator").onchange = function() {
+                    audioBox.children[0].src = this.value;
+                    audioBox.children[0].play();
+                };
+
+                function uploadMp3File() {
+                    var uploadMp3 = document.getElementById("uploadMp3");
+                    if (uploadMp3.files.length===0) {
+                        alert("invalid file");
+                    }
+                    else if (uploadMp3.files[0].type === "audio/mp3") {
+
+                    }
+                }
+            </script>
         </footer>
     </body>
 </html>
--- scripts/main.js
+++ scripts/main.js
@@ -7,41 +7,10 @@ var ctx = visualizer.getContext("2d");
 var audioBox = player.children[1];
 
 var analyser, audioCtx, source;
-var samples = [
-    "samples/Arctic Monkeys – Do I Wanna Know_.mp3",
-    "samples/Gary Clark Jr. – Come Together.mp3",
-    "samples/The Who – My Generation.mp3",
-    "samples/The Damned – Neat Neat Neat.mp3",
-    "samples/Barry White – Never, Never Gonna Give Ya Up.mp3",
-    "samples/Foster The People – Pumped up Kicks.mp3",
-    "samples/Linkin Park – In The End.mp3",
-    "samples/Nirvana – Smells like teen spirit (Original).mp3",
-    "samples/Queen – Brighton Rock.mp3",
-    "samples/The Jon Spenser Blues Explosion – Bellbottoms.mp3"
-];
-
-
-
-function nextSample(t) {
-    var len = samples.length-1;
-    var newSample = samples[Math.round(Math.random()*len)];
-    while(decodeURIComponent(t.src)==window.location+newSample) {
-        newSample = samples[Math.round(Math.random()*len)];
-    }
-    t.src = newSample;
-    t.play();
-}
-
-visualizer.onclick = function() {
-    nextSample(audioBox.children[0]);
-};
-audioBox.children[0].onended = function() {
-    nextSample(audioBox.children[0]);
-};
 
 window.addEventListener("load", initMp3Player, false);
 function initMp3Player() {
-    nextSample(audioBox.children[0]);
+    
     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     analyser = audioCtx.createAnalyser();
     source = audioCtx.createMediaElementSource(audioBox.children[0]);
@@ -64,15 +33,15 @@ function visualize() {
         ctx.fillRect(0, 0, visualizer.width, visualizer.height);
     }
 
-    var cols = 100;
+    var cols = 500;
     if (detectmob()) {
         cols = 40;
     }
-    colWidth = 3;
+    colWidth = 0.5;
     var offset = (visualizer.width-cols*colWidth)/(cols+1);
-    var k = (visualizer.height/2)/255;
+    var k = (visualizer.height*2/3)/255;
 
-    ctx.strokeStyle = "black";
+    ctx.strokeStyle = "red";
     ctx.lineWidth = colWidth;
     ctx.beginPath();
     for (var i=0;i<cols;++i) {
@@ -102,7 +71,7 @@ function detectmob() {
  || navigator.userAgent.match(/iPod/i)
  || navigator.userAgent.match(/BlackBerry/i)
  || navigator.userAgent.match(/Windows Phone/i)
- ){
+ ) {
     return true;
   }
  else {
